a979d2755fd37607c3aef076f69ed63f18441e35
Gabriele Musco <emaildigabry@gmail.com>
2019-07-15 11:36:14 +0200

    completed appdata file translation

--- po/update_potfiles.sh
+++ po/update_potfiles.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 rm *.pot
-find ../hydrapaper -iname "*.py" | xargs xgettext --from-code=UTF-8 --output=hydrapaper-python.pot
-find ../data/ui -iname "*.glade" -or -iname "*.xml" | xargs xgettext --from-code=UTF-8 --output=hydrapaper-glade.pot -L Glade
-find ../data/ -iname "*.desktop.in" | xargs xgettext --from-code=UTF-8 --output=hydrapaper-desktop.pot -L Desktop
-msgcat --use-first hydrapaper-python.pot hydrapaper-glade.pot hydrapaper-desktop.pot > hydrapaper.pot
 version=$(fgrep "version: " ../meson.build | grep -v "meson" | grep -o "'.*'" | sed "s/'//g")
-echo $version
+find ../hydrapaper -iname "*.py" | xargs xgettext --package-name=HydraPaper --package-version=$version --from-code=UTF-8 --output=hydrapaper-python.pot
+find ../data/ui -iname "*.glade" -or -iname "*.xml" | xargs xgettext --package-name=HydraPaper --package-version=$version --from-code=UTF-8 --output=hydrapaper-glade.pot -L Glade
+find ../data/ -iname "*.desktop.in" | xargs xgettext --package-name=HydraPaper --package-version=$version --from-code=UTF-8 --output=hydrapaper-desktop.pot -L Desktop
+find ../data/ -iname "*.appdata.xml.in" | xargs xgettext --no-wrap --package-name=HydraPaper --package-version=$version --from-code=UTF-8 --output=hydrapaper-appdata.pot
+msgcat --use-first hydrapaper-python.pot hydrapaper-glade.pot hydrapaper-desktop.pot hydrapaper-appdata.pot > hydrapaper.pot
 sed 's/#: //g;s/:[0-9]*//g;s/\.\.\///g' <(fgrep "#: " hydrapaper.pot) | sort | uniq > POTFILES.in
 echo "# Please keep this list alphabetically sorted" > LINGUAS
 for l in $(ls *.po); do basename $l .po >> LINGUAS; done
